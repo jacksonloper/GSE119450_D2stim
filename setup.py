c8ed8b0aff5630dda23611efb9ec57f8d64bd0f4
Jackson Loper <jl5116@columbia.edu>
2021-10-22 10:55:59 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='GSE119450_D2stim_anndata',
+    author='',
+    version='0.0.1',
+    include_package_data=True,
+    description='',
+    packages=[
+        'GSE119450_D2stim_anndata',
+    ],
+    package_data={
+        '': ['*.hdf5']
+    },
+    install_requires=[
+        'GitPython'
+    ]
+)
+
+def pull_first():
+    import os
+    import git
+    cwd = os.getcwd()
+    gitdir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(gitdir)
+    g = git.cmd.Git(gitdir)
+    try:
+        g.execute(['git', 'lfs', 'pull'])
+    except git.exc.GitCommandError:
+        raise RuntimeError("Make sure git-lfs is installed!")
+    os.chdir(cwd)
+pull_first()
